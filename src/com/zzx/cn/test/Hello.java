72f4abeecacb82b031f962e78676b622eb06e734
zhengzhixiong <429649446@qq.com>
2016-04-12 14:09:40 +0800

    我的修改

---
+++ src/com/zzx/cn/test/Hello.java
@@ -0,0 +1,15 @@
+package com.zzx.cn.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Hello extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		System.out.println("hahahhahahhah");
+		System.out.println("zheng");
+	}
+}
